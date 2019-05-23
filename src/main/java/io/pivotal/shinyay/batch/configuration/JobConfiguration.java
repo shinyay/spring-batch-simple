8285413e0b6f373476c72b0a4228339f9d581a92
shinyay <shinya.com@gmail.com>
2019-05-23 23:25:14 +0900

    Add Spring Batch Job and Step configuration

---
+++ src/main/java/io/pivotal/shinyay/batch/configuration/JobConfiguration.java
@@ -0,0 +1,39 @@
+package io.pivotal.shinyay.batch.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+
+    public JobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> STEP 1");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job transitionSimpleJobNext() {
+        return jobBuilderFactory.get("transitionJobNext")
+                .start(step1())
+                .build();
+    }
+}
