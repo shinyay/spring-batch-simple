2c6045c6e31cda19f095b6be1ab569256478641a
shinyay <shinya.com@gmail.com>
2019-05-26 19:42:22 +0900

    Add flow definition

--- src/main/java/io/pivotal/shinyay/batch/configuration/JobConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/JobConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -48,12 +50,26 @@ public class JobConfiguration {
                 }).build();
     }
 
+//    @Bean
+//    public Job transitionSimpleJobNext() {
+//        return jobBuilderFactory.get("transitionJobNext")
+//                .start(step1())
+//                .next(step2())
+//                .next(step3())
+//                .build();
+//    }
+
     @Bean
-    public Job transitionSimpleJobNext() {
-        return jobBuilderFactory.get("transitionJobNext")
+    public Job transitionSimpleFlow() {
+        Flow flow = new FlowBuilder<Flow>("simple-flow")
                 .start(step1())
                 .next(step2())
                 .next(step3())
+                .build();
+
+        return jobBuilderFactory.get("transitionJobNext")
+                .start(flow)
+                .end()
                 .build();
     }
 }
