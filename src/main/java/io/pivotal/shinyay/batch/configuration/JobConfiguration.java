3ee0166fcf07cc1773e5f9eaaedf77bc1ac479eb
shinyay <shinya.com@gmail.com>
2019-05-26 21:14:17 +0900

    Modify annotation for Spring Batch enbaling definiion

--- src/main/java/io/pivotal/shinyay/batch/configuration/JobConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/JobConfiguration.java
@@ -2,7 +2,6 @@ package io.pivotal.shinyay.batch.configuration;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
 public class JobConfiguration {
 
     private JobBuilderFactory jobBuilderFactory;
