ebd9ea3f3e13745b34b2e3e38812a8b9399d479c
shinyay <shinya.com@gmail.com>
2019-05-27 20:10:44 +0900

    Add nested child job

--- src/main/java/io/pivotal/shinyay/batch/configuration/ChildJobConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/ChildJobConfiguration.java
@@ -1,8 +1,14 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ChildJobConfiguration {
 
     private JobBuilderFactory jobBuilderFactory;
@@ -11,5 +17,21 @@ public class ChildJobConfiguration {
     public ChildJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step childStep() {
+        return stepBuilderFactory.get("child-step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">> CHILD-STEP");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job childJob() {
+        return jobBuilderFactory.get("child-job")
+                .start(childStep())
+                .build();
     }
 }
