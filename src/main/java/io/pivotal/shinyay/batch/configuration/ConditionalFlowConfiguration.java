488c6d6cd0ab5637c8fa3b87a78fa858b0687745
shinyay <shinya.com@gmail.com>
2019-05-27 15:16:35 +0900

    Add batch job template

---
+++ src/main/java/io/pivotal/shinyay/batch/configuration/ConditionalFlowConfiguration.java
@@ -0,0 +1,37 @@
+package io.pivotal.shinyay.batch.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConditionalFlowConfiguration {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+
+    public ConditionalFlowConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("start-step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("START-TASKLET");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Job conditionalJob() {
+        return jobBuilderFactory.get("conditional-job")
+                .start(startStep())
+                .build();
+    }
+}
