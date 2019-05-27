f9f19cfe75106877d7bc971546998e9e68e0ceef
shinyay <shinya.com@gmail.com>
2019-05-27 19:14:16 +0900

    Add JobExecutionDecider implement logic

--- src/main/java/io/pivotal/shinyay/batch/configuration/decider/EvenOddDecider.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/decider/EvenOddDecider.java
@@ -6,8 +6,16 @@ import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 
 public class EvenOddDecider implements JobExecutionDecider {
+    int executionCount = 0;
+
     @Override
     public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
-        return null;
+        executionCount++;
+
+        if(executionCount % 2 == 0) {
+            return new FlowExecutionStatus("EVEN");
+        } else {
+            return new FlowExecutionStatus("ODD");
+        }
     }
 }
