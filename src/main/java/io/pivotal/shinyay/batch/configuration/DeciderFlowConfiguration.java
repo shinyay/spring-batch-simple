2c59dd3b9dd185cfb0533182eeeed2bc452087b9
shinyay <shinya.com@gmail.com>
2019-05-27 19:30:16 +0900

    Add flow definition based on JobExecutionDecider

--- src/main/java/io/pivotal/shinyay/batch/configuration/DeciderFlowConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/DeciderFlowConfiguration.java
@@ -1,9 +1,11 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import io.pivotal.shinyay.batch.configuration.decider.EvenOddDecider;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,9 +31,36 @@ public class DeciderFlowConfiguration {
     }
 
     @Bean
+    public JobExecutionDecider deciderEvenOdd() {
+        return new EvenOddDecider();
+    }
+
+    @Bean
+    public Step evenStep() {
+        return stepBuilderFactory.get("even-step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("EVEN-TASKLET");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("odd-step")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("ODD-TASKLET");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
     public Job conditionalJob() {
         return jobBuilderFactory.get("decider-job")
                 .start(startStep())
+                .next(deciderEvenOdd())
+                .from(deciderEvenOdd()).on("EVEN").to(evenStep())
+                .from(deciderEvenOdd()).on("ODD").to(oddStep())
+                .end()
                 .build();
     }
 }
