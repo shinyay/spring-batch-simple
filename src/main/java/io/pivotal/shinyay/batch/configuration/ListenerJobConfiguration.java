9d0bafc97a83b5d0ae3e7286e1b641ad2771d043
shinyay <shinya.com@gmail.com>
2019-05-27 23:15:41 +0900

    Add ItemReader and ItemWriter implementation

--- src/main/java/io/pivotal/shinyay/batch/configuration/ListenerJobConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/ListenerJobConfiguration.java
@@ -1,8 +1,15 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class ListenerJobConfiguration {
@@ -14,4 +21,15 @@ public class ListenerJobConfiguration {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
     }
+
+    @Bean
+    public ItemReader<String> reader() {
+        return new ListItemReader<String>(Arrays.asList("One","Two","Three"));
+    }
+
+    @Bean
+    public ItemWriter<String> writer() {
+        return items -> items.forEach(System.out::println);
+    }
+
 }
