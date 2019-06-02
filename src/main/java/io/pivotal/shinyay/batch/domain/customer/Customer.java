90bcd2ed5ddba13fb5d07325447e008525ea0385
shinyay <shinya.com@gmail.com>
2019-06-03 07:27:30 +0900

    Add Customer model

---
+++ src/main/java/io/pivotal/shinyay/batch/domain/customer/Customer.java
@@ -0,0 +1,60 @@
+package io.pivotal.shinyay.batch.domain.customer;
+
+import java.util.Date;
+
+public class Customer {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+
+    public Customer(long id, String firstName, String lastName, Date birthdate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
+}
