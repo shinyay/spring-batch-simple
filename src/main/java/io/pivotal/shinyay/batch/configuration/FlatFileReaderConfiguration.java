86d58c032f0d4e7b00d2c3be34bd04bf5ea841dc
shinyay <shinya.com@gmail.com>
2019-06-08 10:19:57 +0900

    Add Job and Step wirh FlatFileReader

--- src/main/java/io/pivotal/shinyay/batch/configuration/FlatFileReaderConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/FlatFileReaderConfiguration.java
@@ -2,8 +2,11 @@ package io.pivotal.shinyay.batch.configuration;
 
 import io.pivotal.shinyay.batch.configuration.file.CustomerFieldMapper;
 import io.pivotal.shinyay.batch.domain.customer.Customer;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
@@ -25,14 +28,14 @@ public class FlatFileReaderConfiguration {
     }
 
     @Bean
-    public FlatFileItemReader<Customer> itemReader() {
+    public FlatFileItemReader<Customer> fileItemReader() {
         FlatFileItemReader<Customer> reader = new FlatFileItemReader<>();
         reader.setLinesToSkip(1);
         reader.setResource(new ClassPathResource("customer.csv"));
 
         DefaultLineMapper<Customer> mapper = new DefaultLineMapper<>();
         DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
-        tokenizer.setNames(new String[] {"id", "firstName", "lastName", "birthdate"});
+        tokenizer.setNames("id", "firstName", "lastName", "birthdate");
 
         mapper.setLineTokenizer(tokenizer);
         mapper.setFieldSetMapper(new CustomerFieldMapper());
@@ -41,5 +44,26 @@ public class FlatFileReaderConfiguration {
         reader.setLineMapper(mapper);
 
         return reader;
+    }
+
+    @Bean
+    public ItemWriter<? super Object> itemWriter() {
+        return items -> items.forEach(System.out::println);
+    }
+
+    @Bean
+    public Step fileReaderStep() {
+        return stepBuilderFactory.get("csv-reader-step")
+                .chunk(10)
+                .reader(fileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job fileReaderJob() {
+        return jobBuilderFactory.get("csv-reader-job")
+                .start(fileReaderStep())
+                .build();
     }
 }
