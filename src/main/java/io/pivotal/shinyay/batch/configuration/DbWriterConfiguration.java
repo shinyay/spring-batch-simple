f5504bb79779ef365df5598612e4986d8ca0548b
shinyay <shinya.com@gmail.com>
2019-06-12 08:02:46 +0900

    Add itemwriter with JdbcBatchItemWriter and JdbcBatchItermWriterBuilder

---
+++ src/main/java/io/pivotal/shinyay/batch/configuration/DbWriterConfiguration.java
@@ -0,0 +1,40 @@
+package io.pivotal.shinyay.batch.configuration;
+
+import io.pivotal.shinyay.batch.domain.customer.Customer;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbWriterConfiguration {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private DataSource dataSource;
+
+    public DbWriterConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    private FlatFileItemReader<Customer> fileItemReader;
+
+    @Bean
+    public JdbcBatchItemWriter<Customer> jdbcBatchItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Customer>()
+                .dataSource(dataSource)
+                .sql("INSERT INTO CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)")
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Customer>())
+                .build();
+    }
+}
