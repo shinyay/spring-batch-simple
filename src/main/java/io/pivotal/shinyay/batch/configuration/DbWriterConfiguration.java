5d470aeee7d10ecdf089b46b4f30ed8c509d06c5
shinyay <shinya.com@gmail.com>
2019-06-12 21:17:36 +0900

    Add JdbcBatchItemWriter Step

--- src/main/java/io/pivotal/shinyay/batch/configuration/DbWriterConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/DbWriterConfiguration.java
@@ -1,10 +1,11 @@
 package io.pivotal.shinyay.batch.configuration;
 
 import io.pivotal.shinyay.batch.domain.customer.Customer;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,11 +31,20 @@ public class DbWriterConfiguration {
     private FlatFileItemReader<Customer> fileItemReader;
 
     @Bean
-    public JdbcBatchItemWriter<Customer> jdbcBatchItemWriter() {
+    public ItemWriter<Customer> jdbcBatchItemWriter() {
         return new JdbcBatchItemWriterBuilder<Customer>()
                 .dataSource(dataSource)
                 .sql("INSERT INTO CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)")
                 .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Customer>())
+                .build();
+    }
+
+    @Bean
+    public Step dbWriterStep() {
+        return stepBuilderFactory.get("db-writer-step")
+                .<Customer, Customer>chunk(10)
+                .reader(fileItemReader)
+                .writer(jdbcBatchItemWriter())
                 .build();
     }
 }
