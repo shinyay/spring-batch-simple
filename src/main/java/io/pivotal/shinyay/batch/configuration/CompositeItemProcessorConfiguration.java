667ceb31795994ec6057050c9f73c429f3f040ca
shinyay <shinya.com@gmail.com>
2019-06-17 08:10:19 +0900

    Add JdbcPagingItemReader as injection bean

--- src/main/java/io/pivotal/shinyay/batch/configuration/CompositeItemProcessorConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/CompositeItemProcessorConfiguration.java
@@ -1,7 +1,10 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import io.pivotal.shinyay.batch.domain.customer.Customer;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -18,5 +21,7 @@ public class CompositeItemProcessorConfiguration {
         this.stepBuilderFactory = stepBuilderFactory;
         this.dataSource = dataSource;
     }
-    
+
+    @Autowired
+    private JdbcPagingItemReader<Customer> jdbcPagingItemReaderWithBuilder;
 }
