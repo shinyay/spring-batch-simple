afd2b779dbf0a565adac1146d951c315497bf854
shinyay <shinya.com@gmail.com>
2019-06-17 23:46:19 +0900

    Add CompositeItemProcessor Configuratin with FilteringItemProcessor and UpperCaseItemProcessor

--- src/main/java/io/pivotal/shinyay/batch/configuration/CompositeItemProcessorConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/CompositeItemProcessorConfiguration.java
@@ -1,13 +1,23 @@
 package io.pivotal.shinyay.batch.configuration;
 
 import io.pivotal.shinyay.batch.domain.customer.Customer;
+import io.pivotal.shinyay.batch.processor.FilteringItemProcessor;
+import io.pivotal.shinyay.batch.processor.UpperCaseItemProcessor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.support.CompositeItemProcessor;
+import org.springframework.batch.item.support.builder.CompositeItemProcessorBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Configuration
 public class CompositeItemProcessorConfiguration {
@@ -24,4 +34,32 @@ public class CompositeItemProcessorConfiguration {
 
     @Autowired
     private JdbcPagingItemReader<Customer> jdbcPagingItemReaderWithBuilder;
+    @Autowired
+    private FilteringItemProcessor filteringItemProcessor;
+    @Autowired
+    private UpperCaseItemProcessor upperCaseItemProcessor;
+
+    @Bean
+    public CompositeItemProcessor<Customer, Customer> compositeItemProcessor() {
+        return new CompositeItemProcessorBuilder<Customer, Customer>()
+                .delegates(Stream.of(filteringItemProcessor, upperCaseItemProcessor).collect(Collectors.toList()))
+                .build();
+    }
+
+    @Bean
+    public Step compositeItemProcessorStep() {
+        return stepBuilderFactory.get("composite-processor-step")
+                .<Customer, Customer>chunk(10)
+                .reader(jdbcPagingItemReaderWithBuilder)
+                .processor(compositeItemProcessor())
+                .writer(items -> items.forEach(item -> System.out.println(">> Processed: " + item.toString())))
+                .build();
+    }
+
+    @Bean
+    public Job compositeItemProcessorJob() {
+        return jobBuilderFactory.get("composite-processor-job")
+                .start(compositeItemProcessorStep())
+                .build();
+    }
 }
