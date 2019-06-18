c8b7ef9efbe8b09301fd1cfa7d129810dad82082
shinyay <shinya.com@gmail.com>
2019-06-19 07:36:00 +0900

    Add Step and Job for tasklet with restart process

--- src/main/java/io/pivotal/shinyay/batch/configuration/RestartTaskletConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/RestartTaskletConfiguration.java
@@ -1,7 +1,10 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -26,10 +29,33 @@ public class RestartTaskletConfiguration {
                 return RepeatStatus.FINISHED;
             } else {
                 System.out.println(">>> JOB is not Running");
-                chunkContext.getStepContext().getStepExecutionContext().put("STARTED", true);
+                chunkContext.getStepContext().getStepExecution().getExecutionContext().put("STARTED", true);
+//                chunkContext.getStepContext().getStepExecutionContext().put("STARTED", true);
                 throw new RuntimeException("Not Started");
             }
         };
+    }
+
+    @Bean
+    public Step restartTaskletStep1() {
+        return stepBuilderFactory.get("restart-tasklet-step1")
+                .tasklet(restartTasklet())
+                .build();
+    }
+
+    @Bean
+    public Step restartTaskletStep2() {
+        return stepBuilderFactory.get("restart-tasklet-step2")
+                .tasklet(restartTasklet())
+                .build();
+    }
+
+    @Bean
+    public Job restartTaskletJob() {
+        return jobBuilderFactory.get("restart-tasklet-job")
+                .start(restartTaskletStep1())
+                .next(restartTaskletStep2())
+                .build();
     }
 
 }
