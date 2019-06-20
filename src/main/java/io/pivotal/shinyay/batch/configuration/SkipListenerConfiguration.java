69fc794a5f04ea74c20bb09308bc3cbaf34a887d
shinyay <shinya.com@gmail.com>
2019-06-21 07:53:03 +0900

    Add step method with the definition of skippo procedure

--- src/main/java/io/pivotal/shinyay/batch/configuration/SkipListenerConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/SkipListenerConfiguration.java
@@ -1,5 +1,10 @@
 package io.pivotal.shinyay.batch.configuration;
 
+import io.pivotal.shinyay.batch.configuration.itemprocessor.SkipItemProcessor;
+import io.pivotal.shinyay.batch.configuration.itemwriter.SkipItemWriter;
+import io.pivotal.shinyay.batch.configuration.listener.CustomerSkipListener;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.support.ListItemReader;
@@ -29,4 +34,23 @@ public class SkipListenerConfiguration {
                         .collect(Collectors.toList()));
     }
 
+    @Bean
+    public SkipItemProcessor skipItemProcessor() { return new SkipItemProcessor();}
+
+    @Bean
+    public SkipItemWriter skipItemWriter() { return new SkipItemWriter();}
+
+    @Bean
+    public Step skipListenerStep() {
+        return stepBuilderFactory.get("skip-listener-step")
+                .<String, String>chunk(10)
+                .reader(listItemReader())
+                .processor(skipItemProcessor())
+                .writer(skipItemWriter())
+                .faultTolerant()
+                .skip(RuntimeException.class)
+                .skipLimit(15)
+                .listener(new CustomerSkipListener())
+                .build();
+    }
 }
