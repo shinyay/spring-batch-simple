f2da485e5236c3cc9df8792f9ff6c2baef0cb7de
shinyay <shinya.com@gmail.com>
2019-06-24 23:29:36 +0900

    Add AsyncItemProcessor processor method

--- src/main/java/io/pivotal/shinyay/batch/configuration/AsyncItemProcessorConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/AsyncItemProcessorConfiguration.java
@@ -2,11 +2,13 @@ package io.pivotal.shinyay.batch.configuration;
 
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AsyncItemProcessorConfiguration {
@@ -22,12 +24,24 @@ public class AsyncItemProcessorConfiguration {
     @Autowired
     ListItemReader<String> listStringItemReader;
 
+    @Autowired
+    ThreadPoolTaskExecutor threadPoolExecutor;
+
     @Bean
-    ItemProcessor<String, String> slowItemProcessor() {
+    public ItemProcessor<String, String> slowItemProcessor() {
         return item -> {
             Thread.sleep(1000);
             System.out.println("[" + Thread.currentThread().getName() + "]::" + item);
             return item.toUpperCase();
         };
+    }
+
+    @Bean
+    public AsyncItemProcessor<String, String> asyncItemProcessor() throws Exception {
+        AsyncItemProcessor<String, String> asyncItemProcessor = new AsyncItemProcessor<>();
+        asyncItemProcessor.setDelegate(slowItemProcessor());
+        asyncItemProcessor.setTaskExecutor(threadPoolExecutor);
+        asyncItemProcessor.afterPropertiesSet();
+        return asyncItemProcessor;
     }
 }
