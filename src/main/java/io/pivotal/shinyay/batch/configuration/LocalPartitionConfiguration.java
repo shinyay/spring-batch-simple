c51ef4df0218feac6a4038b76773e40e97616ae7
shinyay <shinya.com@gmail.com>
2019-06-26 21:03:14 +0900

    Add JdbcBatchItemWriter with Builder Class

--- src/main/java/io/pivotal/shinyay/batch/configuration/LocalPartitionConfiguration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/LocalPartitionConfiguration.java
@@ -5,7 +5,10 @@ import io.pivotal.shinyay.batch.domain.customer.Customer;
 import io.pivotal.shinyay.batch.domain.customer.CustomerRowMapper;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
 import org.springframework.beans.factory.annotation.Value;
@@ -57,6 +60,15 @@ public class LocalPartitionConfiguration {
                 .rowMapper(new CustomerRowMapper())
                 .queryProvider(queryProvider)
                 .parameterValues(params)
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Customer> jdbcBatchItemWriterNewCustomer() {
+        return new JdbcBatchItemWriterBuilder<Customer>()
+                .dataSource(dataSource)
+                .sql("INSERT INTO NEW_CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)")
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Customer>())
                 .build();
     }
 }
