1090342328737cfe645a1c3bfd496c9e7cc2879e
shinyay <shinya.com@gmail.com>
2019-06-27 21:52:51 +0900

    Add partition hundler with messagingTemplate for MessagingOperations

---
+++ src/main/java/io/pivotal/shinyay/batch/configuration/RemotePartitionConfiguration.java
@@ -0,0 +1,53 @@
+package io.pivotal.shinyay.batch.configuration;
+
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.partition.PartitionHandler;
+import org.springframework.batch.integration.partition.MessageChannelPartitionHandler;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.core.MessagingTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class RemotePartitionConfiguration implements ApplicationContextAware {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private DataSource dataSource;
+    private JobExplorer jobExplorer;
+
+    private ApplicationContext applicationContext;
+    private String customerTableName = "customer";
+    private String customerNetTableName = "new_customer";
+    private int GRID_SIZE = 4;
+
+    public RemotePartitionConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource, JobExplorer jobExplorer) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+        this.jobExplorer = jobExplorer;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public PartitionHandler partitionHandler(MessagingTemplate messagingTemplate) throws Exception {
+        MessageChannelPartitionHandler partitionHandler = new MessageChannelPartitionHandler();
+        partitionHandler.setStepName("slave-step");
+        partitionHandler.setGridSize(GRID_SIZE);
+        partitionHandler.setMessagingOperations(messagingTemplate);
+        partitionHandler.setPollInterval(5000L);
+        partitionHandler.setJobExplorer(jobExplorer);
+        partitionHandler.afterPropertiesSet();
+        return partitionHandler;
+    }
+}
