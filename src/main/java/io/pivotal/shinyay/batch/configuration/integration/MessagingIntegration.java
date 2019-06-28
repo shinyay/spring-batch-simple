32549962eeef23aa52c47ba69763af00eed0ec69
shinyay <shinya.com@gmail.com>
2019-06-28 19:29:35 +0900

    Add ServiceActivator endpoint

--- src/main/java/io/pivotal/shinyay/batch/configuration/integration/MessagingIntegration.java
+++ src/main/java/io/pivotal/shinyay/batch/configuration/integration/MessagingIntegration.java
@@ -1,12 +1,18 @@
 package io.pivotal.shinyay.batch.configuration.integration;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.amqp.outbound.AmqpOutboundEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.core.MessagingTemplate;
 
 @Configuration
 public class MessagingIntegration {
+
+    final static String queueName = "input";
 
     @Bean
     public MessagingTemplate messagingTemplate() {
@@ -18,5 +24,20 @@ public class MessagingIntegration {
     @Bean
     public DirectChannel outboundRequests() {
         return new DirectChannel();
+    }
+
+    @Bean
+    public QueueChannel inboundRequests() {
+        return new QueueChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "outboundRequests")
+    public AmqpOutboundEndpoint amqpOutboundEndpoint(AmqpTemplate amqpTemplate) {
+        AmqpOutboundEndpoint outboundEndpoint = new AmqpOutboundEndpoint(amqpTemplate);
+        outboundEndpoint.setExpectReply(true);
+        outboundEndpoint.setOutputChannel(inboundRequests());
+        outboundEndpoint.setRoutingKey(queueName);
+        return outboundEndpoint;
     }
 }
